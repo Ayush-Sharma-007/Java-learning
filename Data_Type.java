11cb870ff75e2f4903b603db292f5ae2de92266c
Ayush Sharma <ayush120410sharma@gmail.com>
2026-08-31 22:17:20 +0530

    Initial Commit

---
+++ Data_Type.java
@@ -0,0 +1,15 @@
+public class Data_Type {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 2;
+        short c = 14;
+        long d = 1412341;
+
+        float x = 5.6F;
+        double y = 5.6;
+
+        char ch = 'K';
+
+        boolean choice = true;
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte num1 = 3;
+        byte num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);   
+    }
+}
