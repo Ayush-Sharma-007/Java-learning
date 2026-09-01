0855efc8ed4443c186f60d3ec280119fea4f27f1
Ayush Sharma <ayush120410sharma@gmail.com>
2026-09-01 22:41:56 +0530

    Studied Type Conversion

---
+++ 01-Basics/Type_Conversion.java
@@ -0,0 +1,12 @@
+public class Type_Conversion{
+    public static void main(String[] args) {
+        int a = 257;
+        byte k = (byte) a;
+
+        float f = 5.6f;
+        int t = (int) f;
+
+        System.out.println(k);
+        System.out.println(t);
+    }
+}
