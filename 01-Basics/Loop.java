65e26c79d278f5a15b5d86d6ea7d0f45480e956a
Ayush Sharma <ayush120410sharma@gmail.com>
2026-09-02 23:50:38 +0530

    Studied about different types of loops in java

---
+++ 01-Basics/Loop.java
@@ -0,0 +1,26 @@
+public class Loop {
+    public static void main(String[] args) {
+
+        // WHILE LOOP
+
+        int i = 1;
+        while(i<=4){
+            System.out.println("Hi"+i);
+            i++;
+        }   
+
+        // DO WHILE LOOP
+
+        do {
+            System.out.println("Hello World!");
+        }while(false);
+
+
+        // FOR LOOP
+
+        for(int j = 1; j<=4;j++){
+            System.out.println("Hi"+(j-1));
+        }
+
+    }
+}
